bfa4e4773ae31ce5efcb7647082da7d19fe39673
jgchoti <jgchotirat@gmail.com>
2024-05-24 13:01:17 +0200

    Update constants.js

--- data/constants.js
+++ data/constants.js
@@ -1,4 +1,3 @@
-export const API_KEY = '1SlJZB2qLNu2A7eurgQPn8BbBGwc3UEw'
 export const URL_RANDOM = 'https://api.giphy.com/v1/gifs/random?'
 export const URL_SEARCH = 'https://api.giphy.com/v1/gifs/translate?'
 
