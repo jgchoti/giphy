62e730a050d96275200c3a82ad970f9e54034fdf
jgchoti <jgchotirat@gmail.com>
2024-05-24 15:17:02 +0200

    Update get-random.js

--- src/get-random.js
+++ src/get-random.js
@@ -1,6 +1,8 @@
 import { API_KEY } from '../config.js'
 import { URL_RANDOM, DEFAULT_GIPHY } from '../data/constants.js'
 import { changeBg } from "./change-bg.js";
+
+
 export const getRandom = async () => {
     const img = document.querySelector('img')
     try {
