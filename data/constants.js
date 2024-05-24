2558ebeaf12bd1f16906229be26cbfaff84c2395
jgchoti <jgchotirat@gmail.com>
2024-05-24 14:47:08 +0200

    Update constants.js

--- data/constants.js
+++
@@ -1,4 +0,0 @@
-export const URL_RANDOM = 'https://api.giphy.com/v1/gifs/random?'
-export const URL_SEARCH = 'https://api.giphy.com/v1/gifs/translate?'
-
-export const DEFAULT_GIPHY = '../public/search.webp'
