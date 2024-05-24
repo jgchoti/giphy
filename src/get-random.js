2a05068991726429049b50f65782dfa3fc30aedb
jgchoti <jgchotirat@gmail.com>
2024-05-24 14:49:04 +0200

    Update get-random.js

--- src/get-random.js
+++ src/get-random.js
@@ -1,10 +1,9 @@
-import { URL_RANDOM, DEFAULT_GIPHY } from "../data/constants.js";
+import { URL_RANDOM, API_KEY, DEFAULT_GIPHY } from '../config.js'
 import { changeBg } from "./change-bg.js";
-import { config } from "../config.js";
 export const getRandom = async () => {
     const img = document.querySelector('img')
     try {
-        const response = await fetch(`${URL_RANDOM}api_key=${config.API_KEY}`, { mode: 'cors' });
+        const response = await fetch(`${URL_RANDOM}api_key=${API_KEY}`, { mode: 'cors' });
         const data = await response.json()
         img.src = data.data.images.original.url
         changeBg();
