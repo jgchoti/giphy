269db3be512ab8b985821415cf316c3eb761c3ab
jgchoti <jgchotirat@gmail.com>
2024-05-24 14:49:00 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import { getGiphy } from './get-giphy.js'
+import { img } from './img.js'
+import { searchBar } from './search-bar.js'
+import { btnListeners } from './btn-listeners.js'
+
+
+img()
+searchBar()
+btnListeners()
+getGiphy()
