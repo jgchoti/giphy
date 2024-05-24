65b3f6dda0cf55e5a48fddb33a7b84985544c68e
jgchoti <jgchotirat@gmail.com>
2024-05-24 12:46:43 +0200

    Create init.js

---
+++ src/init.js
@@ -0,0 +1,10 @@
+import { getGiphy } from './get-giphy.js'
+import { img } from './img.js'
+import { searchBar } from './search-bar.js'
+import { btnListeners } from './btn-listeners.js'
+import { changeBg } from './change-bg.js'
+
+img()
+searchBar()
+btnListeners()
+getGiphy()
